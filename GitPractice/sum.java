70a2cf58f48710d14a875255e7a8f4dbf2501f18
Vaishnavi Sanjay Padhar <padharvaishnavi02@gmail.com>
2022-10-17 15:18:25 +0530

    initial commit

---
+++ GitPractice/sum.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+class sum{
+
+public static void main(String[] args){
+
+
+Scanner sc = new Scanner(System.in);
+System.out.println("Enter First Number ");
+int a = sc.nextInt();
+System.out.println("First Number is:1 " +a);
+
+System.out.println("Enter Second Number ");
+int b = sc.nextInt();
+System.out.println("Second Number is: " +b);
+
+int c = a + b;
+System.out.println("Sum of First and Second number is: " +c);
+
+}
+}
