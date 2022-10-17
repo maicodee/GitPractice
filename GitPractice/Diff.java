975e0ed92ae51fcff94647db3d6ecb67f454ee56
Vaishnavi Sanjay Padhar <padharvaishnavi02@gmail.com>
2022-10-17 16:17:43 +0530

    Added Diff.java

---
+++ GitPractice/Diff.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+class Diff{
+
+public static void main(String[] args){
+
+
+Scanner sc = new Scanner(System.in);
+System.out.println("Enter First Number ");
+int a = sc.nextInt();
+System.out.println("First Number is:1 " +a);
+4
+System.out.println("Enter Second Number ");
+int b = sc.nextInt();
+System.out.println("Second Number is: " +b);
+
+int c = a - b;
+System.out.println("Difference of First and Second number: " +c);
+
+}
+}
